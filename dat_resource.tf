16954e080837e2503c612a712e00e17a128e36f6
vjagatap <77829130+vjagatap@users.noreply.github.com>
2024-04-08 20:59:40 +0530

    Create dat_resource.tf

---
+++ dat_resource.tf
@@ -0,0 +1,26 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "example" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
